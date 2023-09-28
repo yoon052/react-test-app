7dda15f25013caef0f1efeb44f483ad2cade82ea
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-28 20:11:15 +0900

    test: plus/minus/on-off button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h3 data-testid="counter">{count}</h3>
+        <button
+          data-testid="minus-button"
+          onClick={() => setCount((prev) => prev - 1)}
+          disabled={disabled}
         >
-          Learn React
-        </a>
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          onClick={() => setCount((prev) => prev + 1)}
+          disabled={disabled}
+        >
+          +
+        </button>
+        <button
+          style={{ backgroundColor: "blue" }}
+          data-testId="on/off-button"
+          onClick={() => setDisabled((prev) => !prev)}
+        >
+          on/off
+        </button>
       </header>
     </div>
   );
